abaa7864b670d1d1d2c015bfd964511008cc54dc
usernamedoesntmatter <avtimurmain@gmail.com>
2026-06-17 00:30:16 +0300

    Avoid UB by useing memcpy instead of reinterpret_cast

--- include/Endn/Little.hpp
+++ include/Endn/Little.hpp
@@ -126,7 +126,9 @@ inline std::uint64_t GET_UINT64(const std::uint8_t* buf)
 inline int8_t GET_INT8(const std::uint8_t* buf)
 {
     const std::uint8_t value = GET_UINT8(buf);
-    return *(int8_t*)(&value);
+    std::int8_t output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
@@ -136,8 +138,10 @@ inline int8_t GET_INT8(const std::uint8_t* buf)
  */
 inline int16_t GET_INT16(const std::uint8_t* buf)
 {
-    const std::uint8_t value = GET_UINT8(buf);
-    return *(int16_t*)(&value);
+    const std::uint16_t value = GET_UINT16(buf);
+    std::int16_t output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
@@ -147,8 +151,10 @@ inline int16_t GET_INT16(const std::uint8_t* buf)
  */
 inline int32_t GET_INT32(const std::uint8_t* buf)
 {
-    const std::uint8_t value = GET_UINT8(buf);
-    return *(int32_t*)(&value);
+    const std::uint32_t value = GET_UINT32(buf);
+    std::int32_t output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
@@ -159,8 +165,10 @@ inline int32_t GET_INT32(const std::uint8_t* buf)
  */
 inline int64_t GET_INT48(const std::uint8_t* buf)
 {
-    const std::uint8_t value = GET_UINT8(buf);
-    return *(int64_t*)(&value);
+    const std::uint64_t value = GET_UINT64(buf);
+    std::int64_t output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
@@ -170,8 +178,10 @@ inline int64_t GET_INT48(const std::uint8_t* buf)
  */
 inline int64_t GET_INT64(const std::uint8_t* buf)
 {
-    const std::uint8_t value = GET_UINT8(buf);
-    return *(int64_t*)(&value);
+    const std::uint64_t value = GET_UINT64(buf);
+    std::int64_t output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
@@ -182,7 +192,9 @@ inline int64_t GET_INT64(const std::uint8_t* buf)
 inline float GET_FLOAT32(const std::uint8_t* buf)
 {
     const std::uint32_t value = GET_UINT32(buf);
-    return *(float*)(&value);
+    float output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
@@ -193,7 +205,9 @@ inline float GET_FLOAT32(const std::uint8_t* buf)
 inline double GET_FLOAT64(const std::uint8_t* buf)
 {
     const std::uint64_t value = GET_UINT64(buf);
-    return *(double*)(&value);
+    double output;
+    std::memcpy(&output, &value, sizeof(value));
+    return output;
 }
 
 /**
